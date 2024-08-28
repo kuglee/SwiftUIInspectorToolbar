3ae573d7d8d48d5eb168f4ed63efbbfcb08ecae2
Gábor Librecz <kuglee@gmail.com>
2024-08-28 17:57:34 +0200

    The toolbar buttons of the inspector's view are not visible with the binding inspector, but it's visible with the isPresented inspector

--- SwiftUIInspectorToolbar/ContentView.swift
+++ SwiftUIInspectorToolbar/ContentView.swift
@@ -1,56 +1,33 @@
 import SwiftUI
 import SwiftUINavigation
 
-enum InspectorType: String {
-  case isPresented = "isPresented"
-  case binding = "binding"
-}
-
 struct ContentView: View {
-  @State var inspectorType: InspectorType = .isPresented
-
-  var body: some View {
-    VStack {
-      Button("\(self.inspectorType.rawValue)") {
-        self.inspectorType =
-          switch self.inspectorType {
-          case .isPresented: .binding
-          case .binding: .isPresented
-          }
-      }
-      switch self.inspectorType {
-      case .isPresented: InspectorIsPresented()
-      case .binding: InspectorBinding()
-      }
-    }
-  }
-}
-
-struct InspectorIsPresented: View {
-  @State var isInspectorPresented = false
-
   var body: some View {
     EmptyView()
+
+      // THE TOOLBAR BUTTON IS VISIBLE
       .inspector(isPresented: self.$isInspectorPresented) {
-        Text("Inspector").inspectorColumnWidth(100)
+        Text("Inspector")
+          .toolbar {
+            Spacer()
+            Button(action: { self.isInspectorPresented.toggle() }) {
+              Image(systemName: "sidebar.trailing")
+            }
+          }
       }
-      .toolbar {
-        Button(action: { self.isInspectorPresented.toggle() }) {
-          Image(systemName: "sidebar.trailing")
-        }
-      }
-  }
-}
 
-struct InspectorBinding: View {
+    // THE TOOLBAR BUTTON IS NOT VISIBLE
+    //      .inspector(item: $data) { $data in
+    //        Text("Inspector")
+    //          .toolbar {
+    //            Spacer()
+    //            Button(action: { self.data = if self.data != nil { nil } else { 1 } }) {
+    //              Image(systemName: "sidebar.trailing")
+    //            }
+    //          }
+    //      }
+  }
+
   @State var data: Int?
-
-  var body: some View {
-    EmptyView().inspector(item: $data) { $data in Text("Inspector").inspectorColumnWidth(100) }
-      .toolbar {
-        Button(action: { self.data = if self.data != nil { nil } else { 1 } }) {
-          Image(systemName: "sidebar.trailing")
-        }
-      }
-  }
+  @State var isInspectorPresented = false
 }
